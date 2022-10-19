21c5492365f1d0a5a6bc3e1b5d74c478899b9de1
Rosiele David <rosieleserpa@hotmail.com>
2022-10-19 18:50:16 -0300

    configura temas e estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 function App() {
-  return <div className="App">Coffee Delivery</div>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <div className="App">Coffee Delivery</div>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
 
 export default App
