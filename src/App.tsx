b67bcdf98a18040c2591c90634fa7066f8fd1838
Rosiele David <rosieleserpa@hotmail.com>
2022-11-14 18:13:04 -0300

    add rota de Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
-import { Header } from './components/Header'
+import { Router } from './Router'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
@@ -8,9 +9,9 @@ import { defaultTheme } from './styles/themes/default'
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Header />
-      <div className="App">Coffee Delivery</div>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
