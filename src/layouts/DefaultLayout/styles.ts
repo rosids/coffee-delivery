3d6b113fcd82774f30cb7148e548e49b528afe69
Rosiele David <rosieleserpa@hotmail.com>
2022-11-14 18:12:11 -0300

    add layout padrão

---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  max-width: 90rem;
+  /* height: calc(100vh - 10rem); */
+  margin: auto;
+  padding: 2.5rem;
+
+  border-radius: 8px;
+
+  display: flex;
+  flex-direction: column;
+`
